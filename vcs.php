ca8acdea327323b25bade321f5f66790462f3420
Pauk <paul.khimyack@gmail.com>
2019-12-16 10:05:38 +0200

    vcs script

---
+++ vcs.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pauk
+ * Date: 12/16/19
+ * Time: 8:52 AM
+ */
+$files = glob('vendor/*/*/.git',GLOB_ONLYDIR);
+array_unshift($files, './.git');
+$baseDir  = `pwd`;
+foreach ($files as $file)
+{
+    $file = dirname($file);
+    $fullFile  = realpath($file);
+
+    $res =   `cd $fullFile; git status -s`;
+    if ($res){
+        echo $file . "\n";
+        echo $res;
+    }
+
+}
