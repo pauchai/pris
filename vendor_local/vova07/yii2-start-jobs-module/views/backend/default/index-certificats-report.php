a5b317aa2d9cd83033c90517b891981a326b3538
Pauk <paul.khimyack@gmail.com>
2021-09-09 11:11:06 +0300

    Jobs certificat report revert style

--- vendor_local/vova07/yii2-start-jobs-module/views/backend/default/index-certificats-report.php
+++ vendor_local/vova07/yii2-start-jobs-module/views/backend/default/index-certificats-report.php
@@ -7,21 +7,15 @@ $pageWidth = \vova07\base\helpers\Printer::FORMAT_A5_WIDTH_INCH;
 $pageHeight = \vova07\base\helpers\Printer::FORMAT_A5_HEIGHT_INCH;
 
 $this->registerCss(<<<CSS
-@media print {
-    @page{
-        size: A5 portrait;
+        @page {
+        size: ${pageWidth}in ${pageHeight}in;
+        margin: 6mm
+        }
+    body{
+    font-size:12px
     }
-    body {
-    
-    margin: 6mm;
-    font-size:12px;
-    }
-    
-    
-}
-
 CSS
-);
+        );
 ?>
 
 <?php
